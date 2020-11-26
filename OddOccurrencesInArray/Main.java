65a44a23b81c822286abc5fc6ace998a38f35bfc
matihaw <kontakt.mateusz.hawryluk@gmail.com>
2020-11-26 11:51:24 +0100

    Create Main.java

---
+++ OddOccurrencesInArray/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+	
+	 public static int solution(int[] A) {
+		 int B[] = new int[A.length-1];
+		 for(int a=0; a<A.length;a++) {
+			 for(int i=a+1 ;i<A.length;i++) {
+				if(A[a]>=0) { 
+					if(A[a]==A[i]) {
+						A[a] = -1;
+						A[i] = -1;
+						break;
+					}
+				}
+				
+			 }
+		 }
+		 
+		 for(int c: A) {
+			 if(c>=0) {
+				 return c;
+			 }
+		 }
+		 return -1;
+		 }
+	
+	
+ 
+	  public static void main(String [] args) {
+		  System.out.print(solution(new int[]{9,3,9,3,9,7,9}));
+	  }
+
+}
